cddead841df375d3dc8cd6f31c27300f8e3af1b9
madhurimukund97 <madhurimukund97@msitprogram.net>
2018-11-04 19:27:02 +0530

    "ads2_m6_a1_1: 3/12 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS2-Assignments/module6/ADS-2Week-1/Solution.java
+++ ADS2-Assignments/module6/ADS-2Week-1/Solution.java
@@ -38,6 +38,7 @@ class PageRank {
         lst = new LinearProbingHashST<Integer, Double>();
         // double pas = 0.0;
         double prvalue = 0.0;
+        prlist = new double[dg.vertex()];
         for (int i = 0; i < dg.vertex(); i++) {
             prlist[i] = 1.0 / dg.vertex();
         }
