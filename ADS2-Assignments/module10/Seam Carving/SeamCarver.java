904abdcfd280f30d2fee1b74bb41b67efc4f1355
madhurimukund97 <madhurimukund97@msitprogram.net>
2018-11-08 16:24:46 +0530

    "ads2_m10_a1_1: 3/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS2-Assignments/module10/Seam Carving/SeamCarver.java
+++ ADS2-Assignments/module10/Seam Carving/SeamCarver.java
@@ -1,28 +1,73 @@
-
+import java.awt.Color;
+/**
+ * Class for seam carver.
+ */
 public class SeamCarver {
-	// create a seam carver object based on the given picture.
+	/**
+	 * Picture object.
+	 */
 	Picture pict;
+	/**
+	 * Constructs the object.
+	 * create a seam carver object based on the given picture.
+	 *
+	 * @param      picture  The picture
+	 */
 	public SeamCarver(Picture picture) {
-		
+		if (picture == null) {
+			throw new IllegalArgumentException("picture is null");
+		}
 		this.pict = picture;
 	}
-	// current picture
+	/**
+	 * current picture.
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
 	public Picture picture() {
 		return pict;
 	}
-	// width of current picture
+	/**
+	 * width of current picture.
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
 	public int width() {
 		return pict.width();
 	}
-
-	// height of current picture
+	/**
+	 * height of current picture.
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
 	public int height() {
 		return pict.height();
 	}
-
-	// energy of pixel at column x and row y
+	/**
+	 * energy of pixel at column x and row y.
+	 *
+	 * @param      x     { parameter_description }
+	 * @param      y     { parameter_description }
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
 	public double energy(int x, int y) {
-		return 0;
+		Color left = pict.get(x, y - 1);
+		Color right = pict.get(x, y + 1);
+		Color top = pict.get(x - 1, y);
+		Color bottom = pict.get(x + 1, y);
+		int redh = top.getRed() - bottom.getRed();
+		int greenh = top.getGreen() - bottom.getGreen();
+		int blueh = top.getBlue() - bottom.getBlue();
+		int hor = redh * redh + greenh * greenh + blueh * blueh;
+		int redv = left.getRed() - right.getRed();
+		int greenv = left.getGreen() - right.getGreen();
+		int bluev = left.getBlue() - right.getBlue();
+		int vert = redv * redv + greenv * greenv + bluev * bluev;
+		double energy = Math.sqrt(hor + vert);
+		return energy;
+
+
 	}
 
 	// sequence of indices for horizontal seam
