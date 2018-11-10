d212c19fa20cbc9463f2b5a80ad32560e0012f89
madhurimukund97 <madhurimukund97@msitprogram.net>
2018-11-10 12:40:08 +0530

    "ads2_m12_a1_1: 4/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS2-Assignments/module12/ADS-2_Exam-2/Solution.java
+++ ADS2-Assignments/module12/ADS-2_Exam-2/Solution.java
@@ -61,6 +61,15 @@ public final class Solution {
 			// third is the destination.
 			// If the path exists print the distance between them.
 			// Other wise print "No Path Found."
+			String[] line1 = s.nextLine().split(" ");
+			int src1 = Integer.parseInt(line1[0]);
+			int dest1 = Integer.parseInt(line1[1]);
+			DijkstraUndirectedSP obj1 = new DijkstraUndirectedSP(edg, src1);
+			if(obj1.hasPathTo(dest1)) {
+				System.out.println(obj1.distTo(dest1));
+			} else {
+				System.out.println("No Path Found.");
+			}
 			System.out.println("No Path Found.");
 			break;
 			
