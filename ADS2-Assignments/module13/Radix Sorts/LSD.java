e8dd2dff2146df5dc0f23a75a87b7b1cb193774e
madhurimukund97 <madhurimukund97@msitprogram.net>
2018-11-12 13:48:04 +0530

    "ads2_m13_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS2-Assignments/module13/Radix Sorts/LSD.java
+++ ADS2-Assignments/module13/Radix Sorts/LSD.java
@@ -1,87 +1,47 @@
-/******************************************************************************
- *  Compilation:  javac LSD.java
- *  Execution:    java LSD < input.txt
- *  Dependencies: StdIn.java StdOut.java 
- *  Data files:   https://algs4.cs.princeton.edu/51radix/words3.txt
- *
- *  LSD radix sort
- *
- *    - Sort a String[] array of n extended ASCII strings (R = 256), each of length w.
- *
- *    - Sort an int[] array of n 32-bit integers, treating each integer as 
- *      a sequence of w = 4 bytes (R = 256).
- *
- *  Uses extra space proportional to n + R.
- *
- *
- *  % java LSD < words3.txt
- *  all
- *  bad
- *  bed
- *  bug
- *  dad
- *  ...
- *  yes
- *  yet
- *  zoo
- *
- ******************************************************************************/
-
 /**
- *  The {@code LSD} class provides static methods for sorting an
- *  array of <em>w</em>-character strings or 32-bit integers using LSD radix sort.
- *  <p>
- *  For additional documentation,
- *  see <a href="https://algs4.cs.princeton.edu/51radix">Section 5.1</a> of
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
- *
- *  @author Robert Sedgewick
- *  @author Kevin Wayne
+ * Class for lsd.
  */
 public class LSD {
+    /**
+     * Integer bitsperbyte.
+     */
     private static final int BITS_PER_BYTE = 8;
-
-    // do not instantiate
-    public LSD() { }
-
-   /**  
-     * Rearranges the array of W-character strings in ascending order.
+    /**
+     * Constructs the object.
+     */
+    protected LSD() {
+        // constructor not used.
+    }
+    /**
+     * sort method.
      *
-     * @param a the array to be sorted
-     * @param w the number of characters per string
+     * @param      a     { parameter_description }
+     * @param      w     { parameter_description }
      */
     public static void sort(String[] a, int w) {
         int n = a.length;
         int R = 256;   // extend ASCII alphabet size
         String[] aux = new String[n];
-
         for (int d = w-1; d >= 0; d--) {
-            // sort by key-indexed counting on dth character
-
-            // compute frequency counts
             int[] count = new int[R+1];
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < n; i++) {
                 count[a[i].charAt(d) + 1]++;
-
-            // compute cumulates
-            for (int r = 0; r < R; r++)
+            }
+            for (int r = 0; r < R; r++) {
                 count[r+1] += count[r];
-
-            // move data
-            for (int i = 0; i < n; i++)
+            }
+            for (int i = 0; i < n; i++) {
                 aux[count[a[i].charAt(d)]++] = a[i];
-
-            // copy back
-            for (int i = 0; i < n; i++)
+            }
+            for (int i = 0; i < n; i++) {
                 a[i] = aux[i];
+            }
         }
     }
-
-   
     /**
      * Returns a string representation of the object.
      *
-     * @return     String representation of the object.
+     * @param      a     { parameter_description }
      */
     public void toString(String[] a) {
         StringBuilder sb = new StringBuilder();
@@ -93,28 +53,4 @@ public class LSD {
         sb.append(a[i] + "]");
         System.out.println(sb.toString());
     }
-
-    // /**
-    //  * Reads in a sequence of fixed-length strings from standard input;
-    //  * LSD radix sorts them;
-    //  * and prints them to standard output in ascending order.
-    //  *
-    //  * @param args the command-line arguments
-    //  */
-    // public static void main(String[] args) {
-    //     String[] a = StdIn.readAllStrings();
-    //     int n = a.length;
-
-    //     // check that strings have fixed length
-    //     int w = a[0].length();
-    //     for (int i = 0; i < n; i++)
-    //         assert a[i].length() == w : "Strings must have fixed length";
-
-    //     // sort the strings
-    //     sort(a, w);
-
-    //     // print results
-    //     for (int i = 0; i < n; i++)
-    //         StdOut.println(a[i]);
-    // }
 }
